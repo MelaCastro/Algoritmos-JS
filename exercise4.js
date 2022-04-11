733f66a24e0feef6428cad2fdcc8ac9e08f63983
Melannie Castro <melanniecastrovargas2002@gmail.com>
2022-04-11 11:00:33 -0500

    correction

--- exercise4.js
+++ exercise4.js
@@ -2,29 +2,28 @@
 //¿Cuál es el mayor factor primo del número 600851475143?
 
 function primeFactor(num){
-   result = 1
-   array = []
+  result = 1
+  array = []
 
-  for (let i = 0; i <= Math.sqrt(num); i++){    // si es primo o no
-    if(num % i === 0 && i>result ){
-      array.push(i)
-    }
-  }
-  for (let i = 0; i <array.length; i++){  //comenzar a correr
-    let is_prime = true
-    for (let x = 2; x*x < array[i]; x++){
-      if (array[i] % x === 0){
-        is_prime = false
-        break;
-      }
-    }
-    if (is_prime === true){
-      result = array[i]
-    }
-  }
-  console.log(`el mayor factor primo es: ${result}`)
+ for (let i = 0; i <= Math.sqrt(num); i++){    // si es primo o no
+   if(num % i === 0 && i>result ){
+     array.push(i)
+   }
+ }
+ for (let i = 0; i <array.length; i++){  //comenzar a correr
+   let is_prime = true
+   for (let x = 2; x*x < array[i]; x++){
+     if (array[i] % x === 0){
+       is_prime = false
+       break;
+     }
+   }
+   if (is_prime === true){
+     result = array[i]
+   }
+ }
+ console.log(`el mayor factor primo es: ${result}`)
 }
 
 primeFactor(600851475143)
-
 // problemas: lo hacia demasiado largo y no daba resultado correcto
