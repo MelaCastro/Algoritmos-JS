daa4086412ad912aa5a343bf7b971f19989f7e24
Melannie Castro <melanniecastrovargas2002@gmail.com>
2022-04-13 11:15:24 -0500

    spaces change

--- exercise6.js
+++ exercise6.js
@@ -1,24 +1,24 @@
 //Encuentra el palíndromo más grande hecho del producto de dos números de 3 dígitos.
+
 function palindromoM() {
-  array = []
+  let array = []
   
-  for(i = 999; i>100; i--) {  //inicien en 999 y terminen en 100, multiplicaremos con cada iteracion el numero obtenido 
-    for(x = 999; x>100; x--) {
-      if(palindromo(x*i)){
+  for(i = 999; i > 100; i--) {  //inicien en 999 y terminen en 100, multiplicaremos con cada iteracion el numero obtenido 
+    for(x = 999; x > 100; x--) {
+      if(palindromo(x * i)) {
         array.push(x * i)
       }
     }
   }
-  return Math.max.apply(Math, array);   //elemento mayor en un array numérico.
-
+  return Math.max.apply(Math, array)  //elemento mayor en un array numérico.
 }
 
-function palindromo(p){     //verificar si es palindromo
+function palindromo(p) {  //verificar si es palindromo
   wordReverse = p.toString() === p.toString().split("").reverse().join("")
   return  wordReverse
 }
 
 console.log(`El palíndromo más grande es : ${palindromoM()}`)
 
-//(i = 913 y j = 993)
+//i = 913 y x = 993
 
