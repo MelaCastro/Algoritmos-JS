afa3735d5c73672d4564ae879fa9b7cb0c483768
Melannie Castro <melanniecastrovargas2002@gmail.com>
2022-04-13 10:03:29 -0500

    spaces change

--- exercise1.js
+++ exercise1.js
@@ -1,10 +1,11 @@
 // Múltiplos de 3 o 5
 // Encuentra la suma de todos los múltiplos de 3 o 5 por debajo de 1000.
+
 function sumMultiples () {
-  let sum=0
-  limit = 1000
-    for ( let i = 0; i < limit; i++) {
-      if(i % 3 === 0 || i % 5 === 0){
+  let sum = 0
+  let limit = 1000
+    for( let i = 0; i < limit; i++) {
+      if( i % 3 === 0 || i % 5 === 0) {
        sum = sum + i
       }
     }
