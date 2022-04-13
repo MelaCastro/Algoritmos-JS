6622b1eb8d96121d58987b22e452049170d76b50
Melannie Castro <melanniecastrovargas2002@gmail.com>
2022-04-13 10:50:26 -0500

    spaces change

--- exercise4.js
+++ exercise4.js
@@ -1,29 +1,30 @@
 //Mayor factor primo
 //¿Cuál es el mayor factor primo del número 600851475143?
 
-function primeFactor(num){
-  result = 1
-  array = []
+function primeFactor() {
+  let result = 1
+  let array = []
+  const num = 600851475143
 
- for (let i = 0; i <= Math.sqrt(num); i++){    // si es primo o no
-   if(num % i === 0 && i>result ){
+ for(let i = 0; i <= Math.sqrt(num); i++) {  // si es primo o no
+   if(num % i === 0 && i > result ) {
      array.push(i)
    }
  }
- for (let i = 0; i <array.length; i++){  //comenzar a correr
+ for(let i = 0; i < array.length; i++) {  //encontrando prim
    let is_prime = true
-   for (let x = 2; x*x < array[i]; x++){
-     if (array[i] % x === 0){
+   for(let x = 2; x * x < array[i]; x++) {
+     if(array[i] % x === 0) {
        is_prime = false
        break;
      }
    }
-   if (is_prime === true){
+   if(is_prime === true) {
      result = array[i]
    }
  }
- console.log(`el mayor factor primo es: ${result}`)
+ 
+ console.log(`El mayor factor primo del numero ${num} es: ${result}`)
 }
 
-primeFactor(600851475143)
-// problemas: lo hacia demasiado largo y no daba resultado correcto
+primeFactor()
