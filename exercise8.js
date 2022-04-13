03ef1ff901476ff88dc8ef28fccf865294bd415d
Melannie Castro <melanniecastrovargas2002@gmail.com>
2022-04-13 11:28:50 -0500

    spaces change

--- exercise8.js
+++ exercise8.js
@@ -1,15 +1,17 @@
-//Encuentra la diferencia entre la suma de los cuadrados de los primeros cien números naturales y el cuadrado de la suma.
+// Encuentra la diferencia entre la suma de los cuadrados de los primeros cien 
+// números naturales y el cuadrado de la suma.
 
 function diferencia() {
-  sumaCuadrados = 0
-  suma = 0
-  for (let i = 1; i <= 100; i++) {
-    sumaCuadrados = sumaCuadrados + Math.pow(i,2)
+  let sumaCuadrados = 0
+  let suma = 0
+  const limit = 100
+
+  for (let i = 1; i <= limit; i++) {
+    sumaCuadrados = sumaCuadrados + Math.pow(i, 2)
     suma += i
-  }
-  
-  const cuadradoS = Math.pow(suma,2)
+  }  
+  const cuadradoS = Math.pow(suma, 2)    //  cuadrado de la suma
   return cuadradoS - sumaCuadrados
 }
 
-console.log(` La diferencia es de: ${diferencia()}`)
+console.log(`La diferencia es de: ${diferencia()}`)
